a27bd95b6ef9bc790a79014ddf4d91b522e8615b
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-20 23:07:18 +0300

    feat(component.ts) Set constructor formcontrol to get access to listen for update and validate form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {FormControl, ReactiveFormsModule} from'@angular/forms';
+
 
 @Component({
   selector: 'app-root',
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone:true
 })
 export class AppComponent {
-  title = 'reactiveform';
+  name = new FormControl('');
 }
