a59f603eabf88454efc04e0bd163f421d1c6cfe4
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-21 22:02:28 +0300

    feat(Service): Create a service for logic

--- src/app/servicescustomvalidation.service.ts
+++ src/app/servicescustomvalidation.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicescustomvalidationService {
 
-  constructor() { }
+  patternValidator(): ValidatorFn { 
+    return (control: AbstractControl): ValidationErrors |null => { 
+      
+     return null
+    }
+    const regex = newRegExp('^(?=.*[a-z])(?=.*[A-Z])(?=)
+  }
 }
