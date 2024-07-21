36b60d6f55f1ed477e18798cb4bb4b6eaa093556
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-21 19:58:37 +0300

    feat(Forms): Debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AppComponent,
   ],
   providers: [
     provideClientHydration()
   ],
-  bootstrap: [AppComponent]
+
 })
 export class AppModule { }
+
+
