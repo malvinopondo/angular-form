3adf7fbcad693f6a0e4c44ef17eceb1bae19a354
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-21 23:22:46 +0300

    feat(Service.ts): define the logic of the forms

--- src/app/servicescustomvalidation.service.ts
+++ src/app/servicescustomvalidation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { ValidatorFn, AbstractControl, ValidationErrors, ControlContainer } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 
 
@@ -10,9 +10,49 @@ export class ServicescustomvalidationService {
 
   patternValidator(): ValidatorFn { 
     return (control: AbstractControl): ValidationErrors |null => { 
-      
-     return null
-    }
-    const regex = newRegExp('^(?=.*[a-z])(?=.*[A-Z])(?=)
+    const regex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=')
+    const valid = regex.test(control.value);
+    return valid ? null : { invalidPattern: true };
   }
+};
+
+
+MatchPassword(password: string, confirmPassword: string) {
+  return (formGroup: FormGroup) => {
+    const passwordControl = formGroup.controls[password];
+    const confirmPasswordControl = formGroup.controls [confirmPassword];
+
+    if(!passwordControl || !confirmPasswordControl) {
+      return null;
+    }
+
+    if(confirmPasswordControl.errors && !confirmPasswordControl.errors['passwordMismatch'])
+      return null;  
+
+  if (passwordControl.value !== confirmPasswordControl.value) {
+    confirmPasswordControl.setErrors({ passwordMismatch: true});
+  } else {
+    confirmPasswordControl.setErrors(null);
+  }
+  return null;
 }
+}
+
+userNameValidator(userControl: AbstractControl){
+  return new Promise(resolve => {
+    setTimeout(()=> {
+      if (this.validateUserName(userControl.value)) {
+        resolve({ userNameNotAvailable: true });
+      } else {
+        resolve(null);
+      }
+      }, 1000);
+    });
+  }
+
+  validateUserName (userName: string) {
+    const UserList = ['ankit', 'admin', 'user', 'superuser'];
+    return(UserList.indexOf(userName) > -1);
+  }  
+}
+
